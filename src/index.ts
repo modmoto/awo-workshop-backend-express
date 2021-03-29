fc4b5e4615711280dc6e8b99e0b27864039d2037
Simon Heiss <simonheiss87@gmail.com>
2021-03-29 13:04:01 +0200

    save and load greetings

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,29 @@
 import express from 'express';
+import storage from 'node-persist';
+import { Greeting } from './types';
 
 const app = express();
-app.get('/', (_req, res) => {
-  res.send('Hello Typescript world!');
+app.use(express.json());
+storage.init();
+
+const greetingsKey = 'greetings';
+
+app.get('/greetings', async (_req, res) => {
+  const greetings = await storage.getItem(greetingsKey) ?? [];
+  res.send(greetings);
+});
+
+app.delete('/greetings', async (_req, res) => {
+  await storage.removeItem(greetingsKey);
+  res.send();
+});
+
+app.post('/greetings', async (_req, res) => {
+  const greeting = _req.body as Greeting;
+  const greetings = await storage.getItem(greetingsKey) ?? [];
+  greetings.push(greeting);
+  await storage.setItem(greetingsKey, greetings);
+  res.send(greetings);
 });
 
 const PORT = process.env.PORT || 5000;
---
+++ src/types.ts
@@ -0,0 +1,5 @@
+export interface Greeting {
+    greeting: string,
+    user: string,
+    image: string,
+}
