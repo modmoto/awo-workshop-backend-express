0212b15cc03e3a11decc524ad083b6faefb81824
Simon Heiss <simonheiss87@gmail.com>
2021-04-08 18:06:18 +0200

    fixed serve, added delete and like

--- src/index.ts
+++ src/index.ts
@@ -11,28 +11,39 @@ storage.init();
 
 const greetingsKey = 'greetings';
 
+function createGuid() {
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+    var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+    return v.toString(16);
+  });
+}
+
 app.get('/greetings', async (_req, res) => {
   const greetings = await storage.getItem(greetingsKey) ?? [];
   res.send(greetings);
 });
 
-app.delete('/greetings', async (_req, res) => {
-  await storage.removeItem(greetingsKey);
-  res.send();
+app.delete('/greetings/:greetingId', async (_req, res) => {
+  const greetingId = _req.params.greetingId;
+  const greetings = (await storage.getItem(greetingsKey) ?? []) as Greeting[];
+  const newGreetings = greetings.filter(g => g.id !== greetingId)
+  await storage.setItem(greetingsKey, newGreetings);
+  res.send(newGreetings);
 });
 
 app.post('/greetings', async (_req, res) => {
   const greeting = _req.body as Greeting;
   greeting.likes = 0;
+  greeting.id = createGuid();
   const greetings = await storage.getItem(greetingsKey) ?? [];
   greetings.push(greeting);
   await storage.setItem(greetingsKey, greetings);
   res.send(greetings);
 });
 
-app.put('/greetings', async (_req, res) => {
+app.put('/greetings/:greetingId', async (_req, res) => {
   const greetings = (await storage.getItem(greetingsKey) ?? []) as Greeting[];
-  const greetingId = "123";
+  const greetingId = _req.params.greetingId;
   const greetingEdit = greetings.find(g => g.id === greetingId);
   if (greetingEdit) {
     greetingEdit.likes += 1
